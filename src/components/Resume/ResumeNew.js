28573cc626d4cb2c3cf7121ee009f07c5f38dade
bholedeep <76160240+deepak-bhole@users.noreply.github.com>
2022-07-13 04:35:27 +0530

    update
    asd

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Resume/../Resume/Deepak_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+    AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+
 const resumeLink =
-  "https://deepak-bhole.github.io/src/Resume/Deepak_Resume.pdf";
+  "https://raw.githubusercontent.com/deepak-bhole/deepak-bhole.github.io/main/src/Resume/Deepak_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -21,13 +28,13 @@ function ResumeNew() {
   return (
     <div>
       <Container fluid className="resume-section">
-              <Particle />
+        <Particle />
 
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+              <br />
+              <br />
+              <br />
+              <br />
+              
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
@@ -37,11 +44,82 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download CV pdf version
           </Button>
+              </Row>
+
+              <br />
+              <br />
+              <br />
+              <br />
+         <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="blue">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/deepak-bhole"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/bholedeep07"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/deepak-bhole/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/b_deepak_07/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12} className="home-about-description">
+            <h1 style={{ fontSize: "1.2em" }}>
+              <span className="blue"> Thankyou </span> for visiting my portfolio.
+            </h1>
+
+            <h1 style={{ fontSize: "1em" }}>
+              I hope you have a great day!
+            </h1>
+            
+          </Col>
+          
         </Row>
       </Container>
-    </div>
+      </div>
+
+
   );
 }
 
